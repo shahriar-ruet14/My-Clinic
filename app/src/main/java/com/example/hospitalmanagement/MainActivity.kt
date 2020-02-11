30f1139ac77e0900c096145221243a18a10038bf
Shahriar Mahmud <shahriarbox@gmail.com>
2020-02-11 15:07:07 +0600

    test

--- app/src/main/java/com/example/hospitalmanagement/MainActivity.kt
+++ app/src/main/java/com/example/hospitalmanagement/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.hospitalmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val appointment_button = findViewById(R.id.fab_button) as Button
     }
 }

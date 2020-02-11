ce62d4b50493c5f6a1eea978857e96ebde69a5b2
Shahriar Mahmud <shahriarbox@gmail.com>
2020-02-11 16:41:12 +0600

    Setting a temp View to submit
    This is what i will done with interactive design and with cloud messaging services

--- app/src/main/java/com/example/hospitalmanagement/MainActivity.kt
+++ app/src/main/java/com/example/hospitalmanagement/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hospitalmanagement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 
@@ -10,7 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         val appointment_button = findViewById(R.id.fab_button) as Button
+
+        appointment_button.setOnClickListener {
+            startActivity(Intent(this@MainActivity, Appointment_Activity::class.java))
+        }
     }
 }

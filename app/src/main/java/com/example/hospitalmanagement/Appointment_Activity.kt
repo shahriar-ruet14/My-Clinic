a4e368e223c47216c8854812921fd48b88e671a9
Shahriar Mahmud <shahriarbox@gmail.com>
2020-02-14 22:53:36 +0600

    appointment activity preview created apointment v1

--- app/src/main/java/com/example/hospitalmanagement/Appointment_Activity.kt
+++ app/src/main/java/com/example/hospitalmanagement/Appointment_Activity.kt
@@ -2,11 +2,48 @@ package com.example.hospitalmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class Appointment_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_appointment_)
+
+
+
+        val spinner: Spinner = findViewById(R.id.planets_spinner)
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.category_list,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+
+
+/*
+        val doctors_name: Spinner = findViewById(R.id.doctors_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.doctors_list,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }*/
+
+
+
+
     }
 }
